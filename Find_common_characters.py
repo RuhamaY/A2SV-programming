8ab69b36dd6aead50511629292bb76b56605e199
RuhamaY <168640268+RuhamaY@users.noreply.github.com>
2024-05-15 13:11:48 +0300

    Create Find_common_characters.py

---
+++ Find_common_characters.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution(object):
+    def commonChars(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        common_count = Counter(words[0])
+        
+        for word in words[1:]:
+            word_count = Counter(word)
+            for char in common_count.keys():
+                common_count[char] = min(common_count[char], word_count[char])
+        
+        result = []
+        for char, count in common_count.items():
+            result.extend([char] * count)
+        
+        return result
