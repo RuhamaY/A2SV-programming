0d86abdda4b5eae738dc063ca4c06a4295a11772
Ruhama Yohannes <168640268+RuhamaY@users.noreply.github.com>
2024-05-15 18:54:46 +0300

    Create Counting_sort.py

---
+++ Counting_sort.py
@@ -0,0 +1,13 @@
+def countingSort(arr):
+    frequency = [0] * 100
+    
+    for num in arr:
+        frequency[num] += 1
+    
+    return frequency
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    result = countingSort(arr)
+    print(" ".join(map(str, result)))
