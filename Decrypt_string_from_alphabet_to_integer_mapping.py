c04cc1abc5e66027453e1f1d04579d7ac6d44def
RuhamaY <168640268+RuhamaY@users.noreply.github.com>
2024-05-15 12:34:02 +0300

    Create Decrypt_string_from_alphabet_to_integer_mapping.py

---
+++ Decrypt_string_from_alphabet_to_integer_mapping.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def freqAlphabets(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = []
+        i = 0
+        while i < len(s):
+            if i + 2 < len(s) and s[i + 2] == '#':
+                result.append(chr(int(s[i:i + 2]) + 96))
+                i += 3
+            else:
+                result.append(chr(int(s[i]) + 96))
+                i += 1
+        return ''.join(result)
