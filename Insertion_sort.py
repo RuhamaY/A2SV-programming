8ba390ab98668af81c3b80425592bda9003f3d9c
Ruhama Yohannes <168640268+RuhamaY@users.noreply.github.com>
2024-05-15 18:45:56 +0300

    Create Insertion_sort.py

---
+++ Insertion_sort.py
@@ -0,0 +1,16 @@
+def insertionSort1(n, arr):
+    value = arr[-1]
+    i = n - 2
+    
+    while (i >= 0) and (arr[i] > value):
+        arr[i + 1] = arr[i]
+        print(" ".join(map(str, arr)))
+        i -= 1
+    
+    arr[i + 1] = value
+    print(" ".join(map(str, arr)))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    insertionSort1(n, arr)
